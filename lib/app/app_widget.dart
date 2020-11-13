9a8957e996b65681577c9fb3e48d1eff38f277e0
MARCOS MACHADO MARTINS <marcosm.caixa@gmail.com>
2020-11-13 16:19:07 -0300

    animação do datepicker e finalização

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -5,10 +5,23 @@ class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       navigatorKey: Modular.navigatorKey,
       title: 'Flutter Slidy',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        brightness: Brightness.light,
+        primaryColor: Color.fromARGB(255, 11, 61, 145),
+        accentColor: Color.fromARGB(255, 252, 61, 33),
+        fontFamily: 'Roboto',
+        textTheme: TextTheme(
+          headline6: TextStyle(fontSize: 25, color: Colors.white70),
+          headline4: TextStyle(
+            fontSize: 22,
+            color: Colors.black,
+            fontWeight: FontWeight.bold
+          ),
+          headline2: TextStyle(fontSize: 18, color: Colors.black)
+        ),
       ),
       initialRoute: '/',
       onGenerateRoute: Modular.generateRoute,
--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:nasa_pic_of_day/app/modules/home/models/pic_model.dart';
 import 'home_controller.dart';
+
+import 'package:rive/rive.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key key}) : super(key: key);
@@ -14,14 +16,50 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends ModularState<HomePage, HomeController> {
   //use 'controller' variable to access controller
 
+  Artboard _riveArtboard;
+  RiveAnimationController _controller;
+  @override
+  void initState() {
+    super.initState();
+
+    // Load the animation file from the bundle, note that you could also
+    // download this. The RiveFile just expects a list of bytes.
+    rootBundle.load('assets/calendar.riv').then(
+      (data) async {
+        final file = RiveFile();
+
+        // Load the RiveFile from the binary data.
+        if (file.import(data)) {
+          // The artboard is the root of the animation and gets drawn in the
+          // Rive widget.
+          final artboard = file.mainArtboard;
+          // Add a controller to play back a known animation on the main/default
+          // artboard.We store a reference to it so we can toggle playback.
+          artboard.addController(_controller = SimpleAnimation('animation'));
+          setState(() => _riveArtboard = artboard);
+        }
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('NASA Picture of Day'),
+        title: Text('NASA Picture of Day',
+            style: Theme.of(context).textTheme.headline6),
         actions: <Widget>[
           FlatButton(
-            child: Icon(Icons.calendar_today),
+            child: _riveArtboard == null
+                ? Icon(
+                    Icons.calendar_today,
+                    color: Theme.of(context).accentColor,
+                  )
+                : Container(
+                    width: 50,
+                    height: 50,
+                    child: Rive(artboard: _riveArtboard),
+                  ),
             onPressed: () {
               showDatePicker(
                 context: context,
@@ -43,23 +81,44 @@ class _HomePageState extends ModularState<HomePage, HomeController> {
         ],
       ),
       body: SingleChildScrollView(
-        child: Observer(builder: (_) {
-          return Column(
-            children: <Widget>[
-              Text(
-                  controller.model.title != null ? controller.model.title : ""),
-              controller.model.url != null
-                  ? Image.network(controller.model.url)
-                  : Text(""),
-              Text(controller.model.copyright != null
-                  ? controller.model.copyright
-                  : ""),
-              Text(controller.model.explanation != null
-                  ? controller.model.explanation
-                  : ""),
-            ],
-          );
-        }),
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: Observer(
+            builder: (_) {
+              return Column(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      controller.model.title != null
+                          ? controller.model.title.toUpperCase()
+                          : "",
+                      style: Theme.of(context).textTheme.headline4,
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  controller.model.url != null
+                      ? Image.network(controller.model.url)
+                      : Text(""),
+                  Padding(
+                   padding: EdgeInsets.all(10),
+                    child: Text(controller.model.copyright != null
+                        ? controller.model.copyright
+                        : "",
+                        style: Theme.of(context).textTheme.headline2,),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Text(controller.model.explanation != null
+                        ? controller.model.explanation
+                        : "",
+                        style: Theme.of(context).textTheme.headline2,),
+                  ),
+                ],
+              );
+            },
+          ),
+        ),
       ),
     );
   }
